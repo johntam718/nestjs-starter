f836db92734daf76d5f917ecbb81f6bf1fd925a3
John Tam <jjcmtam@gmail.com>
2023-09-04 22:13:29 +0800

    change model, add config,

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,41 @@
-import { Injectable } from "@nestjs/common";
-
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 @Injectable()
 export class AuthService {
-  signup() {
-    return { msg: "I have signed up" }
+  constructor(private prisma: PrismaService) { }
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        },
+      });
+      delete user.hash;
+      return user
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException("Credntials taken")
+        }
+      }
+      throw error
+    }
   }
-  signin() {
-    return { msg: "I have signed in" }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    if (!user) throw new ForbiddenException("Credenial Incorrect");
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) throw new ForbiddenException("Credenial Incorrect");
+    delete user.hash
+    return user
 
   }
 }
