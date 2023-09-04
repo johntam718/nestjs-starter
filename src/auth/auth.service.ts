beaae8fed07fa953999e074cd00b134f89ef229c
John Tam <jjcmtam@gmail.com>
2023-09-04 22:35:17 +0800

    add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,17 @@ import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { PrismaService } from "src/prisma/prisma.service";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) { }
-  async signup(dto: AuthDto) {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService
+  ) { }
+  async signup(dto: AuthDto):
+    Promise<{ accessToken: string }> {
     const hash = await argon.hash(dto.password);
 
     try {
@@ -16,8 +23,11 @@ export class AuthService {
           hash
         },
       });
-      delete user.hash;
-      return user
+
+      const token = await this.signToken(user.id, user.email)
+      return {
+        accessToken: token
+      }
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === "P2002") {
@@ -28,14 +38,27 @@ export class AuthService {
     }
   }
 
-  async signin(dto: AuthDto) {
+  async signin(dto: AuthDto):
+    Promise<{ accessToken: string }> {
     const user = await this.prisma.user.findUnique({ where: { email: dto.email } });
     if (!user) throw new ForbiddenException("Credenial Incorrect");
 
     const pwMatches = await argon.verify(user.hash, dto.password);
     if (!pwMatches) throw new ForbiddenException("Credenial Incorrect");
-    delete user.hash
-    return user
+    const token = await this.signToken(user.id, user.email)
+    return {
+      accessToken: token
+    }
+  }
 
+  signToken(userId: number, email: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email
+    }
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get("JWT_SECRET")
+    })
   }
 }
